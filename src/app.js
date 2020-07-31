71e7e7efa45cccb5af470525ef8c05f231c301e8
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-08-01 02:38:49 +0300

    define paths for express config

--- src/app.js
+++ src/app.js
@@ -5,3 +5,8 @@ const geoCode = require("./utils/geoCode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
