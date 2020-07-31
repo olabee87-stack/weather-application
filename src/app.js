0713286aee3408b887f90b1abd87aac0070d2adf
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-08-01 02:40:37 +0300

    setup handlebars as the view engine and path to public folder

--- src/app.js
+++ src/app.js
@@ -10,3 +10,11 @@ const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
+
+//Set up handle bars and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Set up static html directory to serve(if HTML)
+app.use(express.static(publicDirectoryPath));
