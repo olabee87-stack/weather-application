a1e892026f279519eca918f0e3b833b09277bb31
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-08-01 02:52:34 +0300

    create client-side app.js to fetch an input address from user

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+console.log("Client side js file");
+
+const form = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-one");
+const messageTwo = document.querySelector("#message-two");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+
+  //Setting the contents of the paragraphs created
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
+  //Get the search value (location) and display the data
+  //If error, dsplay in <p1>. If none, display location in <p1> and forecast in <p2>
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = data.error;
+          //console.log(data.error);
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+        }
+      });
+    }
+  );
+});
