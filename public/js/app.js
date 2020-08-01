d81bba00c8b2f18f19575b32ea1da5e1133b7cac
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-08-01 03:47:10 +0300

    modify fetch url

--- public/js/app.js
+++ public/js/app.js
@@ -15,17 +15,15 @@ form.addEventListener("submit", (e) => {
 
   //Get the search value (location) and display the data
   //If error, dsplay in <p1>. If none, display location in <p1> and forecast in <p2>
-  fetch("http://localhost:3005/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-          //console.log(data.error);
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        //console.log(data.error);
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
